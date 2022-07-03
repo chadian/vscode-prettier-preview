b903244ebf8b8f967f6f65fd16c7c56c611008fc
Chad Carbert <chadcarbert@me.com>
2022-07-03 09:45:34 -0700

    Generate prettier formatted output from open tab to debug console

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as prettier from "prettier";
+import { readFileSync } from "fs";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, "prettier-preview" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "prettier-preview" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "prettier-preview.showPreview",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        "Prettier output generated"
+      );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('prettier-preview.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from prettier-preview!');
-	});
+      const activeEditor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+			const activeFileName = activeEditor?.document?.fileName;
+			console.log(`the active file name is ${activeFileName}`);
 
-	context.subscriptions.push(disposable);
+			if (activeFileName) {
+				const activeContents = readFileSync(activeFileName).toString();
+
+				// TODO: Prettier supports config, this would be nice to pull
+				// from a prettier project config file if it doesn't do/check
+				// this automatically.
+				const formatted = prettier.format(activeContents);
+				console.log(formatted);
+			}
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
